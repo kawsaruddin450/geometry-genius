8c83a26275a96d5503d106226b337672477215ff
kawsar-uddin <kawsaruddin450@gmail.com>
2024-06-16 12:41:41 +0600

    complete area calculation and showing result functionality for all 6 shapes, with input validation

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ function calculateRhombusArea(d1, d2) {
 function calculatePentagonArea(p, b) {
     return 0.5 * p * b;
 }
-function calclateEllipseArea(a, b) {
+function calculateEllipseArea(a, b) {
     return 3.14 * a * b;
 }
 //input field value getting function
@@ -28,9 +28,13 @@ function getInput(inputId1, inputId2) {
     const input2 = inputField2.value;
 
     if (input1 == '' || input2 == '') {
+        inputField1.value = '';
+        inputField2.value = '';
         return "please fill both the fields.";
     }
     else if (isNaN(input1) || isNaN(input2)) {
+        inputField1.value = '';
+        inputField2.value = '';
         return "Please enter a number."
     }
     else {
@@ -47,7 +51,7 @@ function showResult(shapeName, result){
     
     <h2 class="text-xl">${serial}. ${shapeName}</h2>
     <div class="grid grid-cols-2 items-center gap-2">
-        <p class="">${result}cm<sup>2</sup></p>
+        <p class="">${result.toFixed(2)}cm<sup>2</sup></p>
         <button class="bg-blue-500 py-2 px-4 rounded-lg text-white font-medium w-full text-center">cm to m</button>
     </div>
     `
@@ -63,6 +67,71 @@ document.getElementById('triangle-calculate').addEventListener('click', function
         const triangleArea = calculateTriangleArea(inputs[0], inputs[1]);
         showResult("Triangle", triangleArea);
         console.log(triangleArea);
+    }
+    else{
+        alert(inputs);
+    }
+})
+//rectangle
+document.getElementById('rectangle-calculate').addEventListener('click', function (){
+    const inputs = getInput('rectangle-width', 'rectangle-length');
+    if(Array.isArray(inputs)){
+        serial += 1;
+        const rectangleArea = calculateRectangleArea(inputs[0], inputs[1]);
+        showResult("Rectangle", rectangleArea);
+        console.log(rectangleArea);
+    }
+    else{
+        alert(inputs);
+    }
+})
+//perallelogram
+document.getElementById('parallelogram-calculate').addEventListener('click', function (){
+    const inputs = getInput('parallelogram-base', 'parallelogram-height');
+    if(Array.isArray(inputs)){
+        serial += 1;
+        const parallelogramArea = calculatePerallelogramArea(inputs[0], inputs[1]);
+        showResult("Parallelogram", parallelogramArea);
+        console.log(parallelogramArea);
+    }
+    else{
+        alert(inputs);
+    }
+})
+//rhombus
+document.getElementById('rhombus-calculate').addEventListener('click', function (){
+    const inputs = getInput('rhombus-d1', 'rhombus-d2');
+    if(Array.isArray(inputs)){
+        serial += 1;
+        const rhombusArea = calculateRhombusArea(inputs[0], inputs[1]);
+        showResult("Rhombus", rhombusArea);
+        console.log(rhombusArea);
+    }
+    else{
+        alert(inputs);
+    }
+})
+//pentagon
+document.getElementById('pentagon-calculate').addEventListener('click', function (){
+    const inputs = getInput('pentagon-p', 'pentagon-b');
+    if(Array.isArray(inputs)){
+        serial += 1;
+        const pentagonArea = calculatePentagonArea(inputs[0], inputs[1]);
+        showResult("Pentagon", pentagonArea);
+        console.log(pentagonArea);
+    }
+    else{
+        alert(inputs);
+    }
+})
+//ellipse
+document.getElementById('ellipse-calculate').addEventListener('click', function (){
+    const inputs = getInput('ellipse-a', 'ellipse-b');
+    if(Array.isArray(inputs)){
+        serial += 1;
+        const ellipseArea = calculateEllipseArea(inputs[0], inputs[1]);
+        showResult("Ellipse", ellipseArea);
+        console.log(ellipseArea);
     }
     else{
         alert(inputs);
